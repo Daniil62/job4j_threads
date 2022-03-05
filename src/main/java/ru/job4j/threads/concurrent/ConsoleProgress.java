56f54638eec14ba723b4b897f747f259d56151d6
yor_email <daniil.stebnitsky@gmail.com>
2022-03-05 15:49:23 +0300

    3. Прерывание нити [#1019]

---
+++ src/main/java/ru/job4j/threads/concurrent/ConsoleProgress.java
@@ -0,0 +1,33 @@
+package ru.job4j.threads.concurrent;
+
+public class ConsoleProgress implements Runnable {
+
+    private final static String[] PROCESS = new String[]{"\\", "|", "/", "—"};
+    private final static int FREQUENCY = 500;
+    private final static int TIME = 10000;
+
+    @Override
+    public void run() {
+        int index = 0;
+        while (!Thread.currentThread().isInterrupted()) {
+            System.out.print("\r load: " + PROCESS[index++]);
+            index = index == PROCESS.length ? 0 : index;
+            try {
+                Thread.sleep(FREQUENCY);
+            } catch (InterruptedException e) {
+                break;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Thread progress = new Thread(new ConsoleProgress());
+        progress.start();
+        try {
+            Thread.sleep(TIME);
+            progress.interrupt();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
